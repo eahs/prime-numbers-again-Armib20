5013f11ae2faed5cb4a3dcfccdb6a3cd341c18f3
Ali <armib20@gmail.com>
2021-09-30 22:02:36 -0400

    Added Functionality
    Maximum 21 seconds, Minimum 15

--- PrimeNumbersAgain/PrimeNumbersAgain/Program.cs
+++ PrimeNumbersAgain/PrimeNumbersAgain/Program.cs
@@ -25,7 +25,42 @@ namespace PrimeNumbersAgain
 
         static int FindNthPrime(int n)
         {
-            return 0;
+            if (n == 1)
+            {
+                return 2;
+            }
+            
+            int count = 1;
+            int x = 3;
+            int p = 0;
+            
+            while (count < n)
+            {
+                if (isPrime(x) == true)
+                {
+                    count += 1;
+                    p = x;
+                }
+                x += 1;
+            }
+
+            return p;
+        }
+
+        static bool isPrime(int n)
+        {
+            if (n % 2 == 0)
+            {
+                return false;
+            }
+            for (int i = 3; (i * i) <= n; i += 2)
+            {
+                if (((n % i) == 0))
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
         static int GetNumber()
